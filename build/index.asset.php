75f9f659590574a9289e3f2db4b7aacf786139bd
Greg Miller <hello@gregmiller.io>
2023-06-07 17:34:10 -0400

    Render build files
    - Update build files for plugin

--- build/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '29683139122ee7660cfb');
---
+++ build/member.php
@@ -0,0 +1,150 @@
+<?php
+/**
+ * Block template file: member.php
+ *
+ * Membership Block Template.
+ *
+ * @param   array $block The block settings and attributes.
+ * @param   string $content The block inner HTML (empty).
+ * @param   bool $is_preview True during AJAX preview.
+ * @param   (int|string) $post_id The post ID this block is saved to.
+ */
+
+// The get_block_wrapper_attributes() is a requirement. Want think about which new attributes we might need.
+
+$wrapper_attributes = get_block_wrapper_attributes(
+    [
+        'class' => 'member'
+    ]
+);
+?>
+
+
+<section <?= $wrapper_attributes ?>>
+
+    <?php
+
+
+
+    // Repeater
+
+    if ( have_rows( 'add_card' ) ) :
+        while ( have_rows( 'add_card' ) ) : the_row();
+
+            $type_member    = get_sub_field( 'type_member' );
+            $classname      = get_sub_field( 'set_classname' );
+            $add_more       = get_sub_field( 'add_more' );
+
+            ?>
+
+    <article class="card <?= $add_more ? 'bulb' : '';   ?>">
+
+
+            <?php
+
+        // Repeater
+
+            if ( have_rows( 'build_segment' ) ) :
+                while ( have_rows( 'build_segment' ) ) : the_row();
+
+                    $cost               = get_sub_field( 'select_price' );
+                    $description        = get_sub_field( 'description' );
+                    $segment_name       = get_sub_field( 'name_segment' );
+                    $btn_label          = get_sub_field( 'label' );
+                    $add_note           = get_sub_field( 'add_note' );
+                    $cost_note          = get_sub_field( 'cost_note' );
+                    $add_plus           = get_sub_field( 'add_plus' );
+                    $more_label         = get_sub_field( 'more_link_label' );
+                    $more_anchor        = get_sub_field( 'more_link_anchor' );
+                    $form_label         = get_sub_field( 'form_link_label' ); // wording on button
+                    $form_attr          = get_sub_field( 'form_link_title' ); // title attr
+                    $form_url           = get_sub_field( 'form_link_url' );   // link
+
+
+
+                    // assign special character value to $char if $add_plus is true
+                    $add_plus ? $char = '+' : $char = '';
+
+                    ?>
+
+                    <div class="header <?= esc_attr( $classname ); ?>">
+
+                        <h3 class="unit-desc"><?= esc_html( $type_member ) ?></h3>
+
+                    </div>
+
+                    <div class="body">
+                        <?php
+
+                        if ( $segment_name ):
+                            ?>
+                            <h4 class="unit-name"><?= esc_html( $segment_name ) ?></h4>
+
+                        <?php else : ?>
+                            <div class="unit-name"></div>
+                        <?php
+                        endif;
+                        ?>
+
+                        <?php // Code option for price, show a message for flex spaces
+                        if ( $cost ):
+                        ?> 
+                        <div class="unit-cost">
+                        <span class="dollar-sign">$</span><span class="value"><?= esc_attr( $cost ); ?></span><span 
+                                class="plus-char"><?= esc_html( $char ); ?></span><span class="range">/mo</span>
+                        </div>
+                        <?php
+
+                        endif;
+                        if ($add_note ):
+                        ?>
+                        <div class="cost-extra">
+                            <?= $cost_note; ?>
+                        </div>
+                        <?php
+                        endif; ?>
+                        <?php
+                         if ( $description ): ?>
+                            <div class="description"><?= esc_html( $description ); ?>
+
+                            </div>
+
+                        <?php
+                        endif;  ?>
+
+
+                    </div>
+                
+                    <div class="footer">
+                        <?php // add inline anchor
+                        if ( $add_more ): ?>
+                            <a class="cta-link" type="link" href="<?= '#' . esc_attr( $more_anchor ); ?>"
+                               target="_self"><?= esc_html( $more_label ); ?></a>
+                        <?php
+                            endif; ?>
+
+                        <?php
+                        if ( $form_url ):
+                        ?>
+                        <a class="cta-btn" type="link" href="<?= esc_url( $form_url ) ?>" target="_blank"
+                           title="<?= esc_attr( $form_attr ); ?>"><?= esc_html( $form_label ); ?></a>
+                    <?php
+                        endif; ?>
+
+
+                    </div>
+
+
+                <?php endwhile; // build_segment?>
+            <?php else : // build_segment ?>
+                <?php // No rows found ?>
+            <?php endif; // build_segment ?>
+
+    </article>
+
+        <?php endwhile; // add_card ?>
+    <?php else : // add_card ?>
+        <?php // No rows found ?>
+    <?php endif; ?>
+
+</section>
